3202a3de61fdf61dc7314b680fb8042330102fcc
Hayward1991 <hayward5391@hotmail.com>
2026-08-13 16:03:28 +0100

    Add CTRL layout imports

--- CtrlApp.kt
+++ CtrlApp.kt
@@ -3,5 +3,7 @@ package com.ctrl.life
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.*
 import androidx.compose.material3.*
+import androidx.compose.ui.Modifier
+import androidx.compose.foundation.layout.padding
 
 @Composable fun CtrlApp(){ }
