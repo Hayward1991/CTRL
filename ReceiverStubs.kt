03ebc4a35b5549f3758763a0da84221b28bfd85c
Hayward1991 <hayward5391@hotmail.com>
2026-08-13 15:56:11 +0100

    Add Android receiver stub

---
+++ ReceiverStubs.kt
@@ -0,0 +1,9 @@
+package com.ctrl.life
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class CtrlAlarmReceiver:BroadcastReceiver(){
+ override fun onReceive(context:Context,intent:Intent){}
+}
