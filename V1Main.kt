e776d7f9f9c662c7eaef5dfeeb5710aaa9b03657
Hayward1991 <hayward5391@hotmail.com>
2026-08-13 16:01:05 +0100

    Add CTRL v1 activity

--- V1Main.kt
+++ V1Main.kt
@@ -3,3 +3,9 @@ package com.ctrl.life
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+
+class MainActivity:ComponentActivity(){
+ override fun onCreate(savedInstanceState:Bundle?){
+  super.onCreate(savedInstanceState)
+ }
+}
