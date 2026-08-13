8710535791306ea7ab5ab3e33f38d5573afba605
Hayward1991 <hayward5391@hotmail.com>
2026-08-13 22:20:09 +0100

    Define Health Connect snapshot

--- HealthBridge.kt
+++ HealthBridge.kt
@@ -1,3 +1,9 @@
 package com.ctrl.life
 
-object HealthBridge
+import android.content.Context
+import androidx.health.connect.client.HealthConnectClient
+
+object HealthBridge{
+ data class Snapshot(val steps:Long,val sleepMinutes:Long,val available:Boolean)
+ fun available(context:Context)=HealthConnectClient.getSdkStatus(context)==HealthConnectClient.SDK_AVAILABLE
+}
