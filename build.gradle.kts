2df6841540f2ba14f82f330f02289258fa462c34
Hayward1991 <hayward5391@hotmail.com>
2026-08-13 22:19:43 +0100

    Add Health Connect client

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,5 +34,6 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.10.0")
+    implementation("androidx.health.connect:connect-client:1.1.0")
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
