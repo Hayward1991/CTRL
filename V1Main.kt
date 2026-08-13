c37489c669a5c261547093ae3c8f23e6755c93df
Hayward1991 <hayward5391@hotmail.com>
2026-08-13 22:08:10 +0100

    Prompt for read-only calendar permission

--- V1Main.kt
+++ V1Main.kt
@@ -1,5 +1,7 @@
 package com.ctrl.life
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -8,6 +10,9 @@ class MainActivity:ComponentActivity(){
  override fun onCreate(savedInstanceState:Bundle?){
   super.onCreate(savedInstanceState)
   CtrlNotifications.create(this)
+  if(checkSelfPermission(Manifest.permission.READ_CALENDAR)!=PackageManager.PERMISSION_GRANTED){
+   requestPermissions(arrayOf(Manifest.permission.READ_CALENDAR),7001)
+  }
   setContent{CtrlTheme{CtrlApp()}}
  }
 }
