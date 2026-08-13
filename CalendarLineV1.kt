d111fe27f186060751f8a3bfc93abd72529a8e64
Hayward1991 <hayward5391@hotmail.com>
2026-08-14 00:03:26 +0100

    Fix calendar row layout imports

--- CalendarLineV1.kt
+++ CalendarLineV1.kt
@@ -1,11 +1,7 @@
 package com.ctrl.life
 
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
